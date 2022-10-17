1c8d9f97bf2f9319c80fb9498cffca0deff724f8
tribagus <tri.bagus21@gmail.com>
2022-10-17 11:23:29 +0700

    change header hero for detail car result

--- src/components/DetailCarResult/index.js
+++ src/components/DetailCarResult/index.js
@@ -1,14 +1,14 @@
 import "./style.css"
 
 //# components
-// import HeaderHero from "../Payment/HeaderHero"
+import HeaderHero from "../Payment/HeaderHero"
 import DetailPaketCard from "./DetailPaketCard/index"
 import DetailCarCard from "./DetailCarCard"
 
 const DetailCarResult = (props) => {
   return (
     <>
-      {/* <HeaderHero /> */}
+      <HeaderHero />
       <div className="detail__result__container">
         <DetailPaketCard />
         <DetailCarCard {...props} />
