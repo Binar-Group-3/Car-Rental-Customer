88d7140da7ab4c74de3faf41fccfae7753be4845
tribagus <tri.bagus21@gmail.com>
2022-10-17 12:55:51 +0700

    change props to import method from detail car page

--- src/pages/DetailCar/index.js
+++ src/pages/DetailCar/index.js
@@ -1,23 +1,16 @@
 import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 //# redux
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import { handleCar } from "../../redux/actions/carAction"
 
 //# components
 import Navigation from "../../components/LandingPage/Navigation"
 import DetailCarResult from "../../components/DetailCarResult"
 import Footer from "../../components/LandingPage/Footer"
-import { NavMenu } from "../../components/LandingPage/Navigation/data"
-import {
-  socialMediaLogo,
-  userGroupIcon,
-  angleLeftIcon,
-} from "../../const/staticData"
 
 const Car = () => {
-  const { car } = useSelector((state) => state.carReducer)
   const dispatch = useDispatch()
   const param = useParams()
 
@@ -26,18 +19,11 @@ const Car = () => {
     dispatch(handleCar(id))
   }, [])
 
-  const props = {
-    car,
-    socialMediaLogo,
-    NavMenu,
-    userGroupIcon,
-    angleLeftIcon,
-  }
   return (
     <div>
-      <Navigation {...props} />
-      <DetailCarResult {...props} />
-      <Footer {...props} />
+      <Navigation />
+      <DetailCarResult />
+      <Footer />
     </div>
   )
 }
