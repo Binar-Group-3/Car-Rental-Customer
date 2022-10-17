fae9fee71c4754eec7683fb75c4de514caf8230c
tribagus <tri.bagus21@gmail.com>
2022-10-17 11:06:51 +0700

    setup redux for rental date

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import carReducer from "./carReducer"
+import rentalDateReducer from "./rentalDateReducer"
 
 const rootReducer = combineReducers({
   carReducer,
+  rentalDateReducer,
 })
 
 export default rootReducer
